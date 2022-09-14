4e2a02cb84ca77e4556ae2d39da68b25deb47a5b
Shide Foo <shidefoo@gmail.com>
2022-09-14 16:39:46 +0800

    Update install metallib script

--- scripts/install_metallib.sh
+++ scripts/install_metallib.sh
@@ -1,10 +1,19 @@
 #! /bin/bash
+
+# See https://kind.sigs.k8s.io/docs/user/loadbalancer/ for instructions to install metallb
+# Use this script to load balancer tooling on bare-metal clusters including kind clusters
+# Requires docker to be installed
+
 set -ex
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.5/config/manifests/metallb-native.yaml
 kubectl wait pods --all -n metallb-system --for=condition=Ready
 
 KIND_SUBNET=$(docker network inspect kind | jq '.[0].IPAM.Config[0].Subnet' | sed 's/"//g')
-docker run --rm troglobit/netcalc netcalc $KIND_SUBNET > /tmp/netcalc_output.txt
+if [[ -z $KIND_SUBNET ]]; then
+  echo "Could not find kind subnet. Please ensure a kind cluster named kind is present"
+  exit 1
+fi
+docker run --rm troglobit/netcalc:v2.1.6 netcalc $KIND_SUBNET > /tmp/netcalc_output.txt
 HOST_MIN=$(cat /tmp/netcalc_output.txt | grep 'HostMin' | awk '{print $3}')
 HOST_MAX=$(cat /tmp/netcalc_output.txt | grep 'HostMax' | awk '{print $3}')
 IP_ADDRESS_RANGE="${HOST_MIN}-${HOST_MAX}"
