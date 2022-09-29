324af0975e92d0f2136dbaebbb12806135bacf02
Shide Foo <shidefoo@gmail.com>
2022-09-29 18:01:15 +0800

    feat(kserve): Add kserve charts (#11)
    * Add kserve chart
    
    * Add cert-manager crd chart
    
    * Add test chart for kserve
    
    * Add separate workflow to test kserve
    
    * Add readme for certmanager-base chart
    
    * Bump ct version to 3.7.0
    
    * Add values-test for github action step
    
    * Fix lint test command
    
    * Removed comment, fix ct command
    
    * Set wait to 180s
    
    * Create isvc in kserve ns instead of default
    
    * Use sklearn model for test chart
    
    * Reduce resources even more
    
    * Add comments to values, common labels
    
    * Add kserve back into config
    
    * Run kserve test on condition
    
    * Fix syntax error in workflow
    
    * Add ci folder for kserve values
    
    * Fix lint-test
    
    * Fix typo in lint-test
    
    * Change chart name in kserve step
    
    * Add trim to charts_changed
    
    * Fix lint-test for kserve step

---
+++ charts/kserve/files/run_test.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+set -ex
+
+which kubectl > /dev/null 2>&1|| { echo "Kubectl not installed"; exit 1; }
+
+MODEL_NAME=sklearn-iris
+cat << EOF > /tmp/kserve.yaml
+apiVersion: "serving.kserve.io/v1beta1"
+kind: "InferenceService"
+metadata:
+  name: $MODEL_NAME
+spec:
+  predictor:
+    sklearn:
+      storageUri: "gs://kfserving-examples/models/sklearn/1.0/model"
+EOF
+
+kubectl apply -f /tmp/kserve.yaml
+
+kubectl wait isvc/$MODEL_NAME --for=condition=Ready --timeout=180s
+
+INPUT_PATH=input.json
+curl https://raw.githubusercontent.com/kserve/kserve/master/docs/samples/v1beta1/sklearn/v1/iris-input.json -o ${INPUT_PATH}
+INPUT_PATH=@./${INPUT_PATH}
+HOSTNAME=$(kubectl get isvc ${MODEL_NAME} -o jsonpath='{.status.url}' | awk -F '//' '{print $2}')
+LOAD_BALANCER_IP=$(kubectl get services -n istio-system istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+output=$(curl -X POST -vv -H "content-type: application/json" ${LOAD_BALANCER_IP}/v1/models/${MODEL_NAME}:predict -d $INPUT_PATH -o /tmp/output.json -w "%{http_code}" -H "Host: $HOSTNAME")
+
+kubectl delete -f /tmp/kserve.yaml
+
+cat /tmp/output.json
+if [[ $output != "200" ]]; then
+  echo "status code $output is not 200"
+  exit 1;
+fi
+echo "Test pass"
+exit 0
