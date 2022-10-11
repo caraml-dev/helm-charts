3ad6dab2c5daacd3cf3dd5e33bffc76892652b80
Mani Teja <pawanmaniteja@gmail.com>
2022-10-11 15:56:21 +0530

    install vault via gdi; remove hooks from setup resources

--- charts/merlin/files/run.sh
+++ charts/merlin/files/run.sh
@@ -29,23 +29,23 @@ validate_parameters() {
 main() {
   validate_parameters "$@"
   CONFIG_MAP_NAME="$1"
-  RELEASE_NAMESPACE="$2"
+  VAULT_RELEASE_NAMESPACE="vault"
   RELEASE_NAME="$3"
   CLUSTER_NAME="$4"
 
-  echo "Waiting for vault pod(pod/$RELEASE_NAME-vault-0) to be ready.."
+  echo "Waiting for vault pod(pod/vault-0) to be ready.."
   sleep 5
-  kubectl wait pod/$RELEASE_NAME-vault-0 --for=condition=ready --timeout=600s
+  kubectl wait pod/vault-0 --namespace=$VAULT_RELEASE_NAMESPACE --for=condition=ready --timeout=600s
 
   # Downgrade to Vault KV secrets engine version 1
-  kubectl exec $RELEASE_NAME-vault-0 --namespace=$RELEASE_NAMESPACE -- vault secrets disable secret
-  kubectl exec $RELEASE_NAME-vault-0 --namespace=$RELEASE_NAMESPACE -- vault secrets enable -version=1 -path=secret kv
+  kubectl exec vault-0 --namespace=$VAULT_RELEASE_NAMESPACE -- vault secrets disable secret
+  kubectl exec vault-0 --namespace=$VAULT_RELEASE_NAMESPACE -- vault secrets enable -version=1 -path=secret kv
 
   # Write cluster credential to be saved in Vault
   cat /scripts/cluster-credential.json > /tmp/cluster-credential.json
-  kubectl cp /tmp/cluster-credential.json $RELEASE_NAME-vault-0:/tmp/cluster-credential.json
-  kubectl exec $RELEASE_NAME-vault-0 --namespace=$RELEASE_NAMESPACE -- vault kv put secret/$CLUSTER_NAME @/tmp/cluster-credential.json
-  kubectl exec $RELEASE_NAME-vault-0 --namespace=$RELEASE_NAMESPACE -- rm /tmp/cluster-credential.json
+  kubectl cp --namespace=$VAULT_RELEASE_NAMESPACE /tmp/cluster-credential.json vault-0:/tmp/cluster-credential.json
+  kubectl exec vault-0 --namespace=$VAULT_RELEASE_NAMESPACE -- vault kv put secret/$CLUSTER_NAME @/tmp/cluster-credential.json
+  kubectl exec vault-0 --namespace=$VAULT_RELEASE_NAMESPACE -- rm /tmp/cluster-credential.json
   kubectl delete configmap $CONFIG_MAP_NAME
   rm /tmp/cluster-credential.json
 
