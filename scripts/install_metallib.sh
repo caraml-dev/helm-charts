1299fda2ce41dbf8290427abb0de0da5d12386f9
ewezy <ziyi.ewe@gojek.com>
2024-07-18 12:36:22 +0800

    Update the location of ipv4 subnet within docker network config

--- scripts/install_metallib.sh
+++ scripts/install_metallib.sh
@@ -8,7 +8,7 @@ set -ex
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.5/config/manifests/metallb-native.yaml
 kubectl wait pods --all -n metallb-system --for=condition=Ready --timeout 500s
 
-KIND_SUBNET=$(docker network inspect kind | jq '.[0].IPAM.Config[0].Subnet' | sed 's/"//g')
+KIND_SUBNET=$(docker network inspect kind | jq '.[0].IPAM.Config[1].Subnet' | sed 's/"//g')
 if [[ -z $KIND_SUBNET ]]; then
   echo "Could not find kind subnet. Please ensure a kind cluster named kind is present"
   exit 1
